90056bf7ca106d5ab648d283bf0c4e125ed3045c
StevenSookhai <StevenSookhai@gmail.com>
2023-05-02 19:11:54 -0400

    preparing for hosting

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -64,12 +64,12 @@ def inject_csrf_token(response):
     return response
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('public', 'favicon.ico')
-    return app.send_static_file('index.html')
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def react_root(path):
+#     if path == 'favicon.ico':
+#         return app.send_static_file('public', 'favicon.ico')
+#     return app.send_static_file('index.html')
 
 # @app.errorhandler(404)
 # def not_found(e):
