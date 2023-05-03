f447e5a5c5fe81999ec030486f6f781365208101
StevenSookhai <StevenSookhai@gmail.com>
2023-05-03 12:42:35 -0400

    trying to fix render

--- Frontend/src/components/AuthForm.jsx
+++ Frontend/src/components/AuthForm.jsx
@@ -31,6 +31,7 @@ const AuthForm = ({ possibleEmail }) => {
       const response = await fetch(url, {
         method: "POST",
         headers: {
+          // "Content-Type": "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
