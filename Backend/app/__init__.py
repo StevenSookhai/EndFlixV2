896677a4995686c9b5263bc756c8a651d5ad4e00
StevenSookhai <StevenSookhai@gmail.com>
2023-05-03 16:03:38 -0400

    trying to fix render yet again

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -25,7 +25,7 @@ login.login_view = 'auth.unauthorized'
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-CORS(app, supports_credentials=True, resources={r'/*' : {'origins': ['http://localhost:5173']}})
+CORS(app, supports_credentials=True, resources={r'/*' : {'origins': ['http://localhost:5173, https://endflixv2.onrender.com']}})
 
 app.cli.add_command(seed_commands)
 app.config.from_object(Config)
